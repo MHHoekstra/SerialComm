58439919e08e8e5d7ed3ad3b0d66c36d15f6a210
Michel Henrique Hoekstra <michel.h.hoekstra@gmail.com>
2019-04-07 00:13:26 -0300

    Initial Commit

---
+++ SerialComm/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SerialComm
+//
+//  Created by Michel Henrique Hoekstra on 07/04/19.
+//  Copyright © 2019 Michel Henrique Hoekstra. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SerialComm/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SerialComm
+//
+//  Created by Michel Henrique Hoekstra on 07/04/19.
+//  Copyright © 2019 Michel Henrique Hoekstra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
